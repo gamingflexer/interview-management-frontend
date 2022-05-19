fd77f9ae29c9e59d8b4b54bed5e0341c5cd72659
ADlv19 <adwaitg02@gmail.com>
2022-05-19 19:33:07 +0530

    NM

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,19 +3,19 @@ import React from "react";
 export default function LandingPage() {
   return (
     <div>
-      <div class="bg-indigo-900 relative overflow-hidden h-screen">
+      <div className="bg-indigo-900 relative overflow-hidden h-screen">
         <img
           src="https://www.tailwind-kit.com/images/landscape/5.svg"
-          class="absolute h-full w-full object-cover"
+          className="absolute h-full w-full object-cover"
         />
-        <div class="inset-0 bg-black opacity-25 absolute"></div>
-        <header class="absolute top-0 left-0 right-0 z-20">
-          <nav class="container mx-auto px-6 md:px-12 py-4">
-            <div class="md:flex justify-between items-center">
-              <div class="flex justify-between items-center">
-                <a href="#" class="text-white">
+        <div className="inset-0 bg-black opacity-25 absolute"></div>
+        <header className="absolute top-0 left-0 right-0 z-20">
+          <nav className="container mx-auto px-6 md:px-12 py-4">
+            <div className="md:flex justify-between items-center">
+              <div className="flex justify-between items-center">
+                <a href="#" className="text-white">
                   <svg
-                    class="w-8 mr-2 fill-current"
+                    className="w-8 mr-2 fill-current"
                     data-name="Capa 1"
                     viewBox="0 0 16.16 12.57"
                   >
@@ -24,45 +24,45 @@ export default function LandingPage() {
                     <path d="M16.16 5.82H0L8.08 0l8.08 5.82z"></path>
                   </svg>
                 </a>
-                <div class="md:hidden">
-                  <button class="text-white focus:outline-none">
-                    <svg class="h-12 w-12" viewBox="0 0 24 24" fill="none">
+                <div className="md:hidden">
+                  <button className="text-white focus:outline-none">
+                    <svg className="h-12 w-12" viewBox="0 0 24 24" fill="none">
                       <path
                         d="M4 6H20M4 12H20M4 18H20"
                         stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       ></path>
                     </svg>
                   </button>
                 </div>
               </div>
-              <div class="hidden md:flex items-center">
-                <a class="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
+              <div className="hidden md:flex items-center">
+                <a className="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
                   About us
                 </a>
-                <a class="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
+                <a className="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
                   Calendar
                 </a>
-                <a class="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
+                <a className="text-lg uppercase mx-3 text-white cursor-pointer hover:text-gray-300">
                   Contact us
                 </a>
               </div>
             </div>
           </nav>
         </header>
-        <div class="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
-          <div class="lg:w-3/5 xl:w-2/5 flex flex-col items-start relative z-10">
-            <span class="font-bold uppercase text-yellow-400">JRAIMS</span>
-            <h1 class="font-bold text-6xl sm:text-7xl text-white leading-tight mt-4">
+        <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+          <div className="lg:w-3/5 xl:w-2/5 flex flex-col items-start relative z-10">
+            <span className="font-bold uppercase text-yellow-400">JRAIMS</span>
+            <h1 className="font-bold text-6xl sm:text-7xl text-white leading-tight mt-4">
               Let's help you
               <br />
               find right talent
             </h1>
             <a
               href="/home"
-              class="block bg-white hover:bg-gray-100 py-3 px-4 rounded-lg text-lg text-gray-800 font-bold uppercase mt-10"
+              className="block bg-white hover:bg-gray-100 py-3 px-4 rounded-lg text-lg text-gray-800 font-bold uppercase mt-10"
             >
               Get Started
             </a>
